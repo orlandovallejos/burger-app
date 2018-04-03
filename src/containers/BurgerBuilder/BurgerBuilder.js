b0b7bf648bcaab7ce717ca7537ce8024407a3f6b
Orlando Vallejos <orlando@zlapps.com>
2018-04-02 21:33:44 -0300

    Added content to the BurguerBuilder. - Created a CSS file for Layout.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
 
 class BurgerBuilder extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Aux>
+        <div>Burger</div>
+        <div>Build controls</div>
+      </Aux>
     );
   }
 }
