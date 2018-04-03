e85bb002298ef5cb0ed926d13b31b87feec2b8d7
Orlando Vallejos <orlando@zlapps.com>
2018-04-03 00:05:44 -0300

    Added state property to the BurgerBuilder component. - Also added a function to map the object properties to an array and display the ingredients.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,25 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+
+  //This is the same than using the "constructor":
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      meat: 2,
+      cheese: 2
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build controls</div>
       </Aux>
     );
