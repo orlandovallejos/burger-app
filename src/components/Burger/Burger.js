22a2870c55d0635c98a993c24986aaffa92d689c
Orlando Vallejos <orlando@zlapps.com>
2018-04-03 06:54:05 -0300

    Added a message when there are no ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])]
         .map((_, i) => {
           return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
         });
-    });
+    })
+    .reduce((prev, curr) => {
+      return prev.concat(curr);
+    }, []);
+
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please, add some ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
