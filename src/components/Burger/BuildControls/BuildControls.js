9b4e284f5aff2095f06aac45b0c546c1dfa74d9c
Orlando Vallejos <orlando@zlapps.com>
2018-04-08 21:04:18 -0300

    Added the first method handler in the BurgerBuilder container. It's a class component, it has business logic. I passed functions to stateless components and used the onClick listener.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,7 +4,7 @@ import BuildControl from './BuildControl/BuildControl'
 
 const controls = [
   { label: 'Salad', type: 'salad' },
-  { label: 'Bacon', type: 'Bacon' },
+  { label: 'Bacon', type: 'bacon' },
   { label: 'Cheese', type: 'cheese' },
   { label: 'Meat', type: 'meat' }
 ];
@@ -13,7 +13,10 @@ const buildControls = (props) => {
     <div className={classes.BuildControls}>
       {
         controls.map((ctrl) => {
-          return <BuildControl key={ctrl.label} label={ctrl.label} />;
+          return <BuildControl 
+            key={ctrl.label} 
+            label={ctrl.label}
+            added={() => props.ingredientAdded(ctrl.type)} />;
         })
       }
     </div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.4,
+  meat: 1.3,
+  cheese: 0.7
+};
+
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -12,18 +19,37 @@ class BurgerBuilder extends Component {
   //This is the same than using the "constructor":
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      meat: 2,
-      cheese: 2
-    }
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const newCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = newCount;
+
+    const price = INGREDIENT_PRICES[type];
+    const newTotalPrice = this.state.totalPrice + price;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newTotalPrice
+    });
   };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
